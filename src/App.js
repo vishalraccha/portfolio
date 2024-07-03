5be020a5e7cc418924e65f7326641fbfea32065d
vishalraccha <vishalraccha5232@gmail.com>
2024-07-03 18:14:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
 import Firstpage from './components/Firstpage'
+import Expertise from './components/Expertise'
 
 function App() {
   return (
     <>
-      <div className="page h-screen w-screen bg-[#1C1C1E]">
+      <div className="page h-full w-full bg-[#1C1C1E] overflow-x-hidden">
         <Navbar/>
-        {/* <Firstpage/> */}
+        <Firstpage/>
+        <Expertise/>
       </div>
     </>
   );
---
+++ src/components/Expertise.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Expertise() {
+  return (
+    <div className="h-96">
+
+      <h1>Expertise</h1>
+    </div>
+  )
+}
+
+export default Expertise
